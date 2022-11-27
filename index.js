0a5045b3814b1d97f34bcd7840d88809961f939a
jjinwo0 <zvzv9808@gmail.com>
2022-11-28 01:32:24 +0900

    Add Authentication with User Token

--- index.js
+++ index.js
@@ -68,7 +68,17 @@ mongoose.connect(config.mongoURI, {
   })
 
   app.get('/api/users/auth', auth, (req, res) => {
-
+    //Authentication이 true여야 위치 가능
+    res.status(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+    })
   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
